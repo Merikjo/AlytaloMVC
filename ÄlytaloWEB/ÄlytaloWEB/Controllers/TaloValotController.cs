bc0caf1dd6fac05c6bc3a0528fb53f1afe974de8
Merikjo <johanna.merikanto@stud.pointcollege.fi>
2016-09-14 11:04:46 +0300

    Älytalon datan 1. siirto GitHubiin.

---
+++ ÄlytaloWEB/ÄlytaloWEB/Controllers/TaloValotController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ÄlytaloWEB.Models;
+
+namespace ÄlytaloWEB.Controllers
+{
+    public class TaloValotController : Controller
+    {
+        private JohaMeriSQL1Entities db = new JohaMeriSQL1Entities();
+
+        // GET: TaloValot
+        public ActionResult Index()
+        {
+            return View(db.TaloValo.ToList());
+        }
+
+        // GET: TaloValot/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaloValo taloValo = db.TaloValo.Find(id);
+            if (taloValo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taloValo);
+        }
+
+        // GET: TaloValot/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TaloValot/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Valo_ID,Huone,ValaisinType,Lamppu_ID,ValoTila_ID,ValonMaara,ValoTilaKirjattu")] TaloValo taloValo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TaloValo.Add(taloValo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(taloValo);
+        }
+
+        // GET: TaloValot/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaloValo taloValo = db.TaloValo.Find(id);
+            if (taloValo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taloValo);
+        }
+
+        // POST: TaloValot/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Valo_ID,Huone,ValaisinType,Lamppu_ID,ValoTila_ID,ValonMaara,ValoTilaKirjattu")] TaloValo taloValo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taloValo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taloValo);
+        }
+
+        // GET: TaloValot/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TaloValo taloValo = db.TaloValo.Find(id);
+            if (taloValo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taloValo);
+        }
+
+        // POST: TaloValot/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TaloValo taloValo = db.TaloValo.Find(id);
+            db.TaloValo.Remove(taloValo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ÄlytaloWEB/ÄlytaloWEB/Models/SaunaLämpöModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÄlytaloWEB.Models
+{
+    public class SaunaLämpöModel
+    {
+
+        public bool Switched { get; set; }
+        public void SaunaOn (int tila)
+        {
+            if (tila == 0)
+            {
+                Switched = false;
+            }
+            else
+            {
+                Switched = true;
+            }
+        }
+    }
+}
